614c1a9d031ac0dc48672fa324ea6641a936e9be
MetLee <met@whu.edu.cn>
2019-10-26 04:24:30 +0800

    Create analyser.py

---
+++ analyser.py
@@ -0,0 +1,68 @@
+import copy
+import json
+
+def merge(list1,list2,index):
+    mergeResult = copy.deepcopy(list1)
+    for item2 in list2:
+        exist = False
+        for item1 in list1:
+            if item2[index] == item1[index]:
+                exist = True
+                break
+        if not exist:
+            mergeResult.append(copy.deepcopy(item2))
+    return mergeResult
+
+def analyser(data,filters=None): #filter应该为一个list
+    if filters:
+        result = {}
+        for filter in filters: #初始化
+            result[filter] = {'amount': 0, 'detail': []}
+
+        for article in data: #计数
+            subjects = article['subject']
+            if subjects:
+                for subject in subjects:
+                    if subject in result:
+                        result[subject]['amount'] += 1
+                        result[subject]['detail'].append(article)
+
+        union = {'amount': 0, 'detail': []}
+        for filter in filters: #取并集
+            union['detail'] = merge(union['detail'], result[filter]['detail'],'articleURL')
+        union['amount'] = len(union['detail'])
+        result['~union'] = union
+
+        return result
+
+    else: #只计数
+        result = {}
+        for article in data:
+            subjects = article['subject']
+            if subjects:
+                for subject in subjects:
+                    if subject in result:
+                        result[subject]['amount'] += 1
+                    else:
+                        result[subject] = {'amount': 1}
+        return result
+
+def main():
+    with open('datas.txt','r') as f:
+        datas = json.load(f)
+
+    #result_Science = analyser(data=datas['Science'])
+    #with open('result_Science.txt','w') as f:
+    #    f.write(json.dumps(result_Science,indent=4))
+    result_Science = []
+
+    result_Nature = analyser(data=datas['Nature'])
+    with open('result_Nature.txt','w') as f:
+        f.write(json.dumps(result_Nature,indent=4))
+
+    result = {'Science': result_Science, 'Nature': result_Nature}
+    with open('result.txt','w') as f:
+        f.write(json.dumps(result,indent=4))
+
+if __name__ == '__main__':
+	main()   
