04f174f1d14ec22c57fda90d4bf356d11b073b0c
MetLee <met@whu.edu.cn>
2019-10-25 18:03:29 +0800

    'Nature' module

--- getSubject.py
+++ getSubject.py
@@ -11,6 +11,16 @@ def getSubject_Science(URL):  #throw(Exception) (C++ type)
         raise(e)
     return subject
 
+def getSubject_Nature(URL):  #throw(Exception) (C++ type)
+    _ = r.get(URL).content
+    try:
+        subjects = re.findall(r'<meta name="WT.z_subject_term" content="(.*?)">',str(_))[0]
+        subjectList = subjects.split(';')
+    except Exception as e:
+        subjectList = None
+        raise(e)
+    return subjectList
+
 def Science():
     with open('datas.txt','r') as f:
         datas = json.load(f)
@@ -32,3 +42,26 @@ def Science():
         with open('datas.txt','w') as f:
             f.write(json.dumps(datas,indent=4))
 
+def Nature():
+    with open('datas.txt','r') as f:
+        datas = json.load(f)
+
+    counter = 0
+    try:
+        for article in datas['Nature']:
+            if  'subject' not in article or article['subject'] == None:
+                result = getSubject_Nature(article['articleURL'])
+                if result:
+                    article['subject'] = result
+                else:
+                    article['subject'] = result
+                counter += 1
+                print(counter)
+    except Exception as e:
+        print(e)
+    finally:
+        with open('datas.txt','w') as f:
+            f.write(json.dumps(datas,indent=4))
+
+if __name__ == '__main__':
+	Nature()
